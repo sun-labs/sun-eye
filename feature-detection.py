35f708ade824eb85c1a8e0538b81b6cfa04f76b6
Victor Ingman <viregistermail@gmail.com>
2019-01-07 14:52:29 +0100

    PoC, finds clouds

--- feature-detection.py
+++ feature-detection.py
@@ -13,6 +13,15 @@ def processImage(img):
     img = cv2.resize(img, (WIDTH, HEIGHT))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     points = cv2.goodFeaturesToTrack(gray, 25, 0.01, 10)
+    # print(points)
+    for i1 in range(len(points)):
+        p = points[i1][0]
+        u1, v1 = int(round(p[0])), int(round(p[1]))
+        cv2.circle(img, (u1, v1), color=(0, 255, 0), radius=3)
+
+    return img
+
+
 
 def initWindow():
     cv2.namedWindow(WIN_NAME, cv2.WINDOW_NORMAL)
@@ -24,9 +33,12 @@ if __name__ == "__main__":
     initWindow()
     cap = cv2.VideoCapture(sys.argv[1])
     
-    success, img = cap.read()
-    while success:
-        img = processImage(img)
-        cv2.imshow(WIN_NAME, img)
-        cv2.waitKey(0)
-        success, img = cap.read()
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret is True:
+            img = processImage(frame)
+            cv2.imshow(WIN_NAME, img)
+            cv2.waitKey(1)
+            success, img = cap.read()
+        else:
+            break
