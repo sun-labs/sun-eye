8ae37f526aa0f5ca4451b6517ddba8fe6ffefb6f
Victor Ingman <viregistermail@gmail.com>
2019-01-15 14:45:39 +0100

    refactored to functional functions, made draw functions

--- feature-detection.py
+++ feature-detection.py
@@ -14,16 +14,21 @@ COLORS = [(128, 128, 0), (255, 0, 128), (0, 255, 255), (0, 255, 0)]
 
 class Frame():
     def __init__(self, img):
-        self.img = img
+        self.img = self.preprocess(img)
         self.pts = None
         self.kps = None
         self.ds = None
-        self.lastMatches = None
+        self.lastMatches = None # from BFMatcher
+
+    def preprocess(self, img):
+        img = cv2.resize(img, (WIDTH, HEIGHT))
+        return img
+
 
     def getTreshImage(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         ttype = cv2.THRESH_BINARY + cv2.THRESH_OTSU
-        ret, thresh = cv2.threshold(gray, 0, 255, ttype)
+        thresh = cv2.threshold(gray, 0, 255, ttype)[1]
 
         kernel = np.ones((3,3), np.uint8)
         bimg = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
@@ -39,21 +44,29 @@ class Frame():
     def getContours(self, img):
         return cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-    def getCloudCount(self, img):
-        contours = self.getContours(img)
+    def getCloudCount(self, img, contours = None):
+        if contours is None:
+            contours = self.getContours(img)
         return len(list(filter(lambda x: len(x) > 30, contours)))
 
     def getCloudsInformation(self):
         bimg = self.getTreshImage(self.img)
         cloudyness = self.getCloudsPercentage(bimg)
         contours = self.getContours(bimg)
-        nClouds = self.getCloudCount(bimg)
+        nClouds = self.getCloudCount(bimg, contours=contours)
 
         return {
             "cloudyness": cloudyness,
             "contours": contours,
             "cloudCount": nClouds
         }
+
+    def getTrackPoints(self):
+        img = self.img.copy()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        points = cv2.goodFeaturesToTrack(gray, 300, 0.01, 10)
+        self.setPts(points)
+        return points
 
     # find keypoints from one image to the next
     def matchWith(self, frame):
@@ -65,15 +78,14 @@ class Frame():
         kp2, des2 = orb.detectAndCompute(img2, None)
 
         self.setKps(kp1)
-        self.setDs(des1)
+        self.setDes(des1)
         frame.setKps(kp2)
-        frame.setDs(des2)
+        frame.setDes(des2)
 
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
         self.lastMatches = bf.match(des1, des2)
-        img3 = None
-        img3 = cv2.drawMatches(img1, kp1, img2, kp2, self.lastMatches[:], flags=2, outImg=img3)
-        return img3
+        return self.lastMatches
+
 
 
     def setPts(self, pts):
@@ -82,64 +94,36 @@ class Frame():
     def setKps(self, kps):
         self.kps = kps
     
-    def setDs(self, ds):
-        self.ds = ds
+    def setDes(self, des):
+        self.des = des
 
 
-def processImage(img):
-    img = img.copy()
-    frame = Frame(img)
-    img = cv2.resize(img, (WIDTH, HEIGHT))
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    points = cv2.goodFeaturesToTrack(gray, 300, 0.01, 10)
-    frame.setPts(points)
-    # print(points)
+def drawTrackpoints(frame):
+    points = frame.getTrackPoints()
+    img = frame.img.copy()
     for i1 in range(len(points)):
         p = points[i1][0]
         u1, v1 = int(round(p[0])), int(round(p[1]))
         cv2.circle(img, (u1, v1), color=(0, 255, 0), radius=3)
 
-    return img, frame
+    return img
 
-def processClouds(img, frame, pc=None, pl=None):
+def drawMatches(f1, f2, matches):
+    img3 = None
+    return cv2.drawMatches(f1.img, f1.kps, f2.img, f2.kps, matches[:], flags=2, outImg=img3)
 
-    frame = frame or Frame(img)
+def drawClouds(frame):
+
     info = frame.getCloudsInformation()
+    img = frame.img.copy()
     for cnt in info["contours"]:
         cv2.drawContours(img,[cnt],0,(0,0,255),2)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img, '{} clouds'.format(info["cloudCount"]),(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(img, '{} clouds'.format(info["cloudCount"]), (10, 100), font, 1, cv2.LINE_AA)
     cv2.putText(img, '{}% cloudy'.format(round(info["cloudyness"] * 100)), (10, 300), font, 2, cv2.LINE_AA)
 
     return img
-
-def processClouds_kmeans(img, frame, pc = None, pl = None):
-    img = cv2.resize(img, (WIDTH, HEIGHT))
-    
-    pts = frame.pts
-    K = 4
-    flags = cv2.KMEANS_RANDOM_CENTERS
-    if pc is not None and pl is not None:
-      print(len(pc), len(pl), K, len(pts))
-    if pl is not None:
-        if len(pl) > len(pts):
-            pl = pl[:len(pts)]
-        else:
-            pl = np.pad(pl, len(pts), 'constant')
-
-    (ret, label, centroid) = cv2.kmeans(pts, K, None, (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 5, 1.0), 5, flags)
-    centroid = np.uint8(centroid)
-    res = centroid[label.flatten()]
-    print(centroid, len(centroid))
-    for i1 in range(len(res)):
-        p = pts[i1].flatten()
-        color = COLORS[int(label[i1])]
-       #  print(p)
-        u1, v1 = (int(round(p[0])), int(round(p[1])))
-        cv2.circle(img, (u1, v1), 3, color)
-
-    return img, None, centroid, label
 
 
 def initWindow():
@@ -157,22 +141,23 @@ if __name__ == "__main__":
     cap = cv2.VideoCapture(sys.argv[1])
     
     pf = None #previous frame
-    pc = None #previous centroids kmeans
-    pl = None # previous kmeans labels
     while cap.isOpened():
         ret, frame = cap.read()
         if ret is True:
-            img, cf = processImage(frame)
-            img = img.copy()
-            imgCloud = processClouds(frame, cf, pc, pl)
+            cf = Frame(frame)
+            imgCloud = drawClouds(cf)
+            imgTrack = drawTrackpoints(cf)
+
+            if pf is not None:
+                matches = cf.matchWith(pf)
+                imgMatch = drawMatches(cf, pf, matches)
+                cv2.imshow(WIN_NAME, imgMatch)
 
             cv2.imshow(WIN_NAME_C, imgCloud)
-            cv2.imshow(WIN_NAME_B, img)
-            if pf is not None:
-                img = cf.matchWith(pf)
+            cv2.imshow(WIN_NAME_B, imgTrack)
+            
             pf = cf
-            cv2.imshow(WIN_NAME, img)
             cv2.waitKey(1)
-            success, img = cap.read()
+            success, frame = cap.read()
         else:
             break
