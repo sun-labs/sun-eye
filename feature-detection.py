173cc491bb7ef16b451eed00e41232217d0b03e7
Victor Ingman <viregistermail@gmail.com>
2019-01-15 16:53:16 +0100

    refactored main logic to suneye class

--- feature-detection.py
+++ feature-detection.py
@@ -9,47 +9,74 @@ import argparse
 from lib.Frame import Frame
 from lib.Display import Display
 
-def parseFrame(frame):
-    cf = Frame(frame)
-    winf.drawClouds(cf)
-    wint.drawTrackpoints(cf)
-    wintresh.imshow(cf.getTreshImage(cf.img))
-    return cf
+class SunEye():
 
-def parseVideo(path):
-    cap = cv2.VideoCapture(args.file_path)
-    winm = Display('matches')
-    pf = None # previous frame
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret is True:
-            cf = parseFrame(frame)
+    def __init__(self, args):
+        self.nogui = args.nogui
+        self.pf = None # previous frame
+        self.initGUI()
+        self.processFiles(args.file_path)
+    
+    def processFiles(self, files):
+        for path in files:
+            if "mp4" in path:
+                self.pf = self.handleVideo(path)
+            else:
+                self.pf = self.handleImage(path)
+                cv2.waitKey(0)
+    
+    def initGUI(self):
+        if self.nogui is False:
+            self.winm = Display('matches')
+            self.winf = Display('features')
+            self.wint = Display('trackers')
+            self.wintresh = Display('tresh')
+
+    def getFrameInfo(self, frame):
+        if self.pf is not None:
+            matches = frame.matchWith(self.pf)
+            cloudInfo = frame.getCloudsInformation()
+
+    def drawFrame(self, frame, matches = None, pf = None):
+        pf = self.pf if pf is None else pf # global previous frame if needed
+        if self.nogui is False:
+            self.winf.drawClouds(frame)
+            self.wint.drawTrackpoints(frame)
+            self.wintresh.imshow(frame.getTreshImage(frame.img))
             if pf is not None:
-                matches = cf.matchWith(pf)
-                winm.drawMatches(cf, pf, matches)
-            pf = cf
-            cv2.waitKey(1)
-            frame = cap.read()[1]
+                matches = matches if matches is not None else frame.matchWith(pf)
+                self.winm.drawMatches(frame, pf, matches)
 
-def parseImage(path):
-    frame = cv2.imread(path)
-    cf = parseFrame(frame)
-    cv2.waitKey(0)
+    def handleVideo(self, path):
+        cap = cv2.VideoCapture(path)
+        pf = None # previous frame
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret is True:
+                cf = Frame(frame)
+                self.drawFrame(cf, pf = pf)
+                    
+                pf = cf
+                cv2.waitKey(1)
+                frame = cap.read()[1]
+        return pf
+
+    def handleImage(self, path):
+        frame = Frame(cv2.imread(path))
+        self.drawFrame(frame)
+        return frame
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Process weather information from frame / video.')
-    parser.add_argument('file_path', metavar='FILE', type=str)
+    parser.add_argument('file_path', metavar='FILE', type=str, nargs='+')
     parser.add_argument('--atemp')
     parser.add_argument('--ahumid')
+    parser.add_argument('--nogui', default=False)
     args = parser.parse_args()
-
-    winf = Display('features')
-    wint = Display('trackers')
-    wintresh = Display('tresh')
-
-    path = args.file_path
-    if "mp4" in path:
-        parseVideo(path)
-    else:
-        parseImage(path)
+    
+    
+    se = SunEye(args)
+    pf = None # previous frame
+    
