80cf068de956b193d3a6445df8ca34270752ce33
Victor Ingman <viregistermail@gmail.com>
2019-01-15 14:22:49 +0100

    refactored matching function to frame class

--- feature-detection.py
+++ feature-detection.py
@@ -18,6 +18,7 @@ class Frame():
         self.pts = None
         self.kps = None
         self.ds = None
+        self.lastMatches = None
 
     def getTreshImage(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -54,6 +55,26 @@ class Frame():
             "cloudCount": nClouds
         }
 
+    # find keypoints from one image to the next
+    def matchWith(self, frame):
+        # Initiate SIFT detector
+        orb = cv2.ORB_create()
+        img1 = self.img.copy()
+        img2 = frame.img.copy()
+        kp1, des1 = orb.detectAndCompute(img1, None)
+        kp2, des2 = orb.detectAndCompute(img2, None)
+
+        self.setKps(kp1)
+        self.setDs(des1)
+        frame.setKps(kp2)
+        frame.setDs(des2)
+
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        self.lastMatches = bf.match(des1, des2)
+        img3 = None
+        img3 = cv2.drawMatches(img1, kp1, img2, kp2, self.lastMatches[:], flags=2, outImg=img3)
+        return img3
+
 
     def setPts(self, pts):
         self.pts = pts
@@ -80,19 +101,6 @@ def processImage(img):
 
     return img, frame
 
-def match_frames(f1, f2):
-    # Initiate SIFT detector
-    orb = cv2.ORB_create()
-    img1 = f1.img
-    img2 = f2.img
-    kp1, des1 = orb.detectAndCompute(f1.img,None)
-    kp2, des2 = orb.detectAndCompute(f2.img,None)
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1,des2)
-    img3 = None
-    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:], flags=2, outImg=img3)
-    return img3
-
 def processClouds(img, frame, pc=None, pl=None):
 
     frame = frame or Frame(img)
@@ -104,7 +112,7 @@ def processClouds(img, frame, pc=None, pl=None):
     cv2.putText(img, '{} clouds'.format(info["cloudCount"]),(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
     cv2.putText(img, '{}% cloudy'.format(round(info["cloudyness"] * 100)), (10, 300), font, 2, cv2.LINE_AA)
 
-    return img, None, None, None
+    return img
 
 def processClouds_kmeans(img, frame, pc = None, pl = None):
     img = cv2.resize(img, (WIDTH, HEIGHT))
@@ -156,13 +164,12 @@ if __name__ == "__main__":
         if ret is True:
             img, cf = processImage(frame)
             img = img.copy()
-            imgCloud, cfCloud, centroids, labels = processClouds(frame, cf, pc, pl)
-            pc = centroids
-            pl = labels
+            imgCloud = processClouds(frame, cf, pc, pl)
+
             cv2.imshow(WIN_NAME_C, imgCloud)
             cv2.imshow(WIN_NAME_B, img)
             if pf is not None:
-                img = match_frames(cf, pf)
+                img = cf.matchWith(pf)
             pf = cf
             cv2.imshow(WIN_NAME, img)
             cv2.waitKey(1)
