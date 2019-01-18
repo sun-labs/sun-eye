881bca7d063a556e7863f83f25622e9eb46b7723
Victor Ingman <viregistermail@gmail.com>
2019-01-18 14:45:14 +0100

    added equations for calculating cloud height and viewport width

--- feature-detection/lib/equations.py
+++ feature-detection/lib/equations.py
@@ -1,2 +1,12 @@
+import math
 # implement trigonometric formulas
 
+def dewPoint(relhumid, airtemp):
+    return airtemp - ((100 - relhumid)/5)
+
+def cloudHeight(relhumid, airtemp):
+    return (relhumid - dewPoint(relhumid, airtemp))/0.00802
+
+def viewportWidth(cloudAltitude, focalAngle):
+    rad = math.radians(focalAngle)
+    return (cloudAltitude * math.tan(rad))/2
