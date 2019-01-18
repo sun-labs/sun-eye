a617d082d35d0d80dd3a73a380f9df9f2cf4f1a4
Victor Ingman <viregistermail@gmail.com>
2019-01-18 14:44:38 +0100

    saving mosaics as files after processed

--- mosaic/mosaic.py
+++ mosaic/mosaic.py
@@ -6,6 +6,7 @@ from minio.error import ResponseError
 import argparse
 import cv2
 import numpy as np
+import math
 
 def parseDate(dateStr):
     return tuple(dateStr.split("-"))
@@ -16,20 +17,24 @@ if __name__ == "__main__":
     #parser.add_argument('--date', type=str)
     #parser.add_argument('--end', type=str)
     parser.add_argument('date', metavar='DATE', type=str)
+    parser.add_argument('--device-name', type=str)
     args = parser.parse_args()
     
     mc = Minio('***REMOVED***',
                   access_key='***REMOVED***',
                   secret_key='***REMOVED***',
                   secure=False)
+    date = args.date
+    y, m, d = parseDate(date)
 
-    y, m, d = parseDate(args.date)
-
-    localPath = './temp/{}'.format(args.date)
-    mcPath = '/***REMOVED***/{}/{}/{}/'.format(y, m, d)
+    deviceName = args.device_name
+    localPath = './temp/{}-{}'.format(deviceName, date)
+    savePath = './mosaics'
+    mcPath = '/{}/{}/{}/{}/'.format(deviceName, y, m, d)
 
     os.makedirs(localPath, exist_ok=True)
-
+    os.makedirs(savePath, exist_ok=True)
+    
     photos = mc.list_objects_v2('***REMOVED***', mcPath)
     for p in photos:
         paths = p.object_name.split('/')
@@ -51,7 +56,9 @@ if __name__ == "__main__":
     mimg = np.zeros((imgCount, W*H, 3))
     # print(mimg)
 
+    print('mosaic in progress...')
     for i in range(imgCount):
+        print('{}% {}/{}'.format(math.floor((i/imgCount)*100), i, imgCount))
         img = images[i]
         cv2.waitKey(1)
         imgPath = "{}/{}".format(localPath, img)
@@ -64,6 +71,9 @@ if __name__ == "__main__":
         mimg[i, :, 1] = mimg[i, :, 1] / 255.0
         mimg[i, :, 2] = mimg[i, :, 2] / 255.0
         
-        cv2.imshow('asdf', mimg)
+        # cv2.imshow('asdf', mimg)
 
+    cv2.imwrite('{}/{}-{}.jpg'.format(savePath, deviceName, date), mimg)
+    print('mosaic done, saved in mosaics')
     cv2.waitKey(0) # last image
+
